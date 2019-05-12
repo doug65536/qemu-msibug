c6b7ec9b6287ea55ce467f20b4b604db573f82f4
Doug Gale <doug16k@gmail.com>
2019-05-12 20:37:58 -0230

    Error handling in bisect script

--- bisect.sh
+++ bisect.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-if [[ -z "$BUILDDIR" ]]; then
+if ! [[ -d "$BUILDDIR" ]]; then
 	echo need BUILDDIR environment to specify qemu build directory
 	echo which must be adjacent to the qemu source directory
 	exit 1
 fi
 
-if [[ -z "$TESTDIR" ]]; then
+if ! [[ -d "$TESTDIR" ]]; then
 	echo need TESTDIR environment to specify qemu-bmibug test case directory
 	echo 'https://github.com/doug65536/qemu-msibug'
 	exit 1
 fi
 
-rm -rfI "$BUILDDIR/*"
-pushd "$BUILDDIR"
-../qemu/configure --prefix="$PWD/out" --target-list=i386-softmmu --enable-virglrenderer
-make -j8 && make install
+rm -rfI "$BUILDDIR/*" || exit 125
+pushd "$BUILDDIR" || exit 125
+../qemu/configure --prefix="$PWD/out" --target-list=i386-softmmu \
+	--enable-virglrenderer || exit 125
+make -j8 && make install || exit 125
 testqemu="$PWD/out/bin/qemu-system-i386"
-pushd "$TESTDIR"
+pushd "$TESTDIR" || exit 125
 QEMU="$testqemu" make run || exit
